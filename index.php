b736e051e85cecef6ecf21b254369c99c8fa20c3
amudide <amudide@gmail.com>
2020-10-20 10:55:44 -0400

    added simple log in

--- index.php
+++ index.php
@@ -3,3 +3,23 @@
 spl_autoload_register(function ($class) {
     include 'classes/' . $class . '.class.php';
 });
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+</head>
+<body>
+    <form action="handler.php" method="post">
+        <p>
+        Username <input type="text" name="username"> <br> <br>
+        Password <input type="text" name="pwd"> <br> <br>
+        <input type="submit" value="Log In!">
+        </p>
+    </form>
+</body>
+</html>
